b0a2e2ee10c93e301e70afe6fb259d9eec595698
yhr20000319 <61374236+yhr20000319@users.noreply.github.com>
2024-03-26 16:42:54 +0800

    Update run.sh

--- IENE/Temporal_Evolution/temp_arxiv/run.sh
+++ IENE/Temporal_Evolution/temp_arxiv/run.sh
@@ -1,11 +1,9 @@
 
 # ogb-arxiv
-
+python main.py --method erm --gnn gcn --lr 0.01 --dataset ogb-arxiv --device 0
 python main.py --method erm --gnn sage --lr 0.01 --dataset ogb-arxiv --device 0
-
-python main.py --method iene --gnn sage --lr 0.005 --K 5 --T 5 --num_sample 1 --beta 0.5 --lr_a 0.01 --dataset ogb-arxiv --device 0
-
 python main.py --method erm --gnn gpr --lr 0.01 --dataset ogb-arxiv --device 0
-
-python main.py --method iene --gnn gpr --lr 0.01 --K 3 --T 5 --num_sample 1 --beta 1.0 --lr_a 0.001 --dataset ogb-arxiv --device 0
+python main.py --method iene --gnn gcn --lr 0.01 --num_sample 1 --beta 0.5 --lr_a 0.01 --dataset ogb-arxiv --device 0 --dropout 0.2 --weight_decay 5e-4
+python main.py --method iene --gnn sage --lr 0.01 --num_sample 1 --beta 0.5 --lr_a 0.01 --dataset ogb-arxiv --device 0 --dropout 0.2 --weight_decay 5e-4
+python main.py --method iene --gnn gpr --lr 0.01 --num_sample 1 --beta 1.0 --lr_a 0.001 --dataset ogb-arxiv --device 0 --dropout 0.2 --weight_decay 5e-4
 
