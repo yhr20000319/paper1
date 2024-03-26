e7cc314759e3c99e91c91cf868c9d337d8bffcd6
yhr20000319 <61374236+yhr20000319@users.noreply.github.com>
2024-03-26 16:47:03 +0800

    Update run.sh

--- IENE/Temporal_Evolution/temp_elliptic/run.sh
+++ IENE/Temporal_Evolution/temp_elliptic/run.sh
@@ -2,9 +2,8 @@
 # elliptic
 
 python main.py --method erm --gnn sage --lr 0.01 --weight_decay 0. --num_layers 5 --dataset elliptic --device 0
-
-python main.py --method iene --gnn sage --lr 0.001 --weight_decay 0. --num_layers 5 --K 3 --T 1 --num_sample 5 --beta 1.0 --lr_a 0.005 --dataset elliptic --device 0
-
 python main.py --method erm --gnn gpr --lr 0.01 --weight_decay 0. --num_layers 5 --dataset elliptic --device 0
-
-python main.py --method iene --gnn gpr --lr 0.01 --weight_decay 0. --num_layers 5 --K 3 --T 1 --num_sample 5 --beta 1.0 --lr_a 0.005 --dataset elliptic --device 0
+python main.py --method iene --gnn gcn --lr 0.001 --weight_decay 0.001 --num_layers 5 --num_sample 5 --beta 1.0 --lr_a 0.005 --dataset elliptic --device 0 --dropout 0.2
+python main.py --method iene --gnn gat --lr 0.001 --weight_decay 5e-4 --num_layers 5 --num_sample 5 --beta 1.0 --lr_a 0.005 --dataset elliptic --device 0 --dropout 0.1
+python main.py --method iene --gnn sage --lr 0.001 --weight_decay 0.001 --num_layers 5 --num_sample 5 --beta 1.0 --lr_a 0.005 --dataset elliptic --device 0 --dropout 0.2
+python main.py --method iene --gnn gpr --lr 0.01 --weight_decay 0.001 --num_layers 5 --num_sample 5 --beta 1.0 --lr_a 0.005 --dataset elliptic --device 0 --dropout 0.2
