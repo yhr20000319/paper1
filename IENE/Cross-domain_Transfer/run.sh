da3b4666ffd63e17c36220441382edf9cefcf1af
yhr20000319 <61374236+yhr20000319@users.noreply.github.com>
2024-03-26 16:36:28 +0800

    Update run.sh

--- IENE/Cross-domain_Transfer/run.sh
+++ IENE/Cross-domain_Transfer/run.sh
@@ -1,18 +1,10 @@
 
 # Twitch-e
 python main.py --dataset twitch-e --method erm --gnn gcn --lr 0.01 --weight_decay 1e-3 --num_layers 2 --device 0
-
-python main.py --dataset twitch-e --method iene --gnn gcn --lr 0.01 --weight_decay 1e-3 --num_layers 2 --K 3 --T 1 --num_sample 5 --beta 3.0 --lr_a 0.001 --device 0
-
 python main.py --dataset twitch-e --method erm --gnn gat --lr 0.01 --weight_decay 1e-3 --num_layers 2 --device 0
-
-python main.py --dataset twitch-e --method iene --gnn gat --lr 0.01 --weight_decay 1e-3 --num_layers 2 --K 3 --T 1 --num_sample 5 --beta 1.0 --lr_a 0.005 --device 0
-
-python main.py --dataset twitch-e --method erm --gnn gcnii --lr 0.01 --weight_decay 1e-3 --num_layers 10 --device 0
-
-python main.py --dataset twitch-e --method iene --gnn gcnii --lr 0.001 --weight_decay 1e-3 --num_layers 10 --K 3 --T 1 --num_sample 5 --beta 1.0 --lr_a 0.01 --device 0
+python main.py --dataset twitch-e --method iene --gnn gcn --lr 0.01 --weight_decay 5e-5 --num_layers 2 --num_sample 5 --beta 3.0 --lr_a 0.001 --device 0 --dropout 0.2
+python main.py --dataset twitch-e --method iene --gnn gat --lr 0.01 --weight_decay 5e-5 --num_layers 2 --num_sample 5 --beta 1.0 --lr_a 0.005 --device 0 --dropout 0.2
 
 # fb-100
 python main.py --dataset fb100 --method erm --gnn gcn --lr 0.01 --weight_decay 1e-3 --num_layers 2 --device 0
-
-python main.py --dataset fb100 --method iene --gnn gcn --lr 0.01 --weight_decay 1e-3 --num_layers 2 --K 3 --T 1 --num_sample 5 --beta 1.0 --lr_a 0.005 --device 0
+python main.py --dataset fb100 --method iene --gnn gcn --lr 0.01 --weight_decay 5e-5 --num_layers 2 --num_sample 5 --beta 1.0 --lr_a 0.005 --device 0 --dropout 0.2
